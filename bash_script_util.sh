32e1d040acddb790f5cc355da5417221882e0bfe
emendes28 <emendes28@users.noreply.github.com>
2021-09-25 20:59:03 -0300

    utils scripts

---
+++ bash_script_util.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket backend-terraform
---
+++ bat_script_util.bat
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket backend-terraform
+terraform init
